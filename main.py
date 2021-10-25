ecaee3a39900aceb87f81ea716ede4f67152106b
Francisco Marfull <marfullsen@gmail.com>
2021-10-25 17:13:39 -0300

    Hito 2: Lee todas las URL de todos los libros de cada página.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ DIR = 'catalogue/'
 # Var.
 i = 1
 req = ''
+line_count = 0
 
 # Fun.
 def get_page(page):
@@ -16,10 +17,41 @@ def get_page(page):
   req = requests.get(f'{HOST}{DIR}{page}')
   return req.status_code
 
+def create_csv_file():
+  with open('scraped_books.csv', mode='w') as scraped_books:
+    fieldnames = ['Title',
+      'Price',
+      'Stock',
+      'Category',
+      'Cover',
+      'UPC',
+      'Product Type',
+      'Price (excl. tax)',
+      'Price (incl. tax)',
+      'Tax',
+      'Availability',
+      'Number of reviews'
+      ]
+    writer = csv.DictWriter(scraped_books, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL, fieldnames=fieldnames)
+    writer.writeheader()
+
+def write_to_csv():
+  global line_count
+  line_count += 1
+
 # Main
+
+# Se inicia el proyecto con una hoja CSV nueva.
+create_csv_file()
+
 while get_page(f'page-{i}.html') != 404:
   soup = BeautifulSoup(req.text, 'lxml')
-  book_link = soup.section.div.nextSibling.nextSibling.ol.li.h3.a.get('href')
-  print(i, req.status_code, book_link)
-  print(i, get_page(book_link))
+  books = soup.section.div.nextSibling.nextSibling.ol.findAll('li')
+  for n, book in enumerate(books):
+    print(n, book.h3.a.get('href'))
+  #book_link = soup.section.div.nextSibling.nextSibling.ol.li.h3.a.get('href')
+  #print(i, req.status_code, book_link)
+  #print(i, get_page(book_link))
   i += 1
+
+print(f'Se procesarion {line_count} libros.')
