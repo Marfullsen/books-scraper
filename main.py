fb7c83956bac6315248c3059589588e184038980
Francisco Marfull <marfullsen@gmail.com>
2021-10-25 21:13:37 -0300

    Refactor: Legibilidad mejorada, 'if __main__' agregado.

--- main.py
+++ main.py
@@ -53,30 +53,34 @@ def init():
   # Se inicia el proyecto con una hoja CSV nueva.
   create_csv_file()
 
-# Main
-init()
+def main():
+  global i, line_count
+  init()
 
-while get_page(f'page-{i}.html') != 404:
-  print(f'Página {i}')
-  soup = BeautifulSoup(req.text, 'lxml')
-  books = soup.section.div.nextSibling.nextSibling.ol.findAll('li')
-  for n, book in enumerate(books):
-    book_req = get_page(book.h3.a.get('href'))
+  while get_page(f'page-{i}.html') != 404:
+    print(f'Página {i}')
     soup = BeautifulSoup(req.text, 'lxml')
-    title = soup.find("h1").text
-    price = soup.find(attrs={"class":"price_color"}).text[2:]
-    stock = only_num(soup.find(attrs={"class":"instock availability"}).text.strip())
-    category = soup.find(attrs={"class":"breadcrumb"}).find("li", class_="active").previous_sibling.previous_sibling.text.strip()
-    cover = soup.find("img")['src'][6:]
-    table_items = soup.find(attrs={"class":"table"}).find_all('td')
-    upc = table_items[0].text
-    product_type = table_items[1].text
-    price_wo_tax = table_items[2].text[2:].strip()
-    price_w_tax = table_items[3].text[2:].strip()
-    tax = table_items[4].text[2:].strip()
-    availability = only_num(table_items[5].text)
-    number_of_reviews = table_items[6].text
-    write_to_csv([title, price, stock, category, cover, upc, product_type, price_wo_tax, price_w_tax, tax, availability, number_of_reviews])
-  i += 1
+    books = soup.section.div.nextSibling.nextSibling.ol.findAll('li')
+    for n, book in enumerate(books):
+      book_req = get_page(book.h3.a.get('href'))
+      soup = BeautifulSoup(req.text, 'lxml')
+      title = soup.find("h1").text
+      price = soup.find(attrs={"class":"price_color"}).text[2:]
+      stock = only_num(soup.find(attrs={"class":"instock availability"}).text.strip())
+      category = soup.find(attrs={"class":"breadcrumb"}).find("li", class_="active").previous_sibling.previous_sibling.text.strip()
+      cover = soup.find("img")['src'][6:]
+      table_items = soup.find(attrs={"class":"table"}).find_all('td')
+      upc = table_items[0].text
+      product_type = table_items[1].text
+      price_wo_tax = table_items[2].text[2:].strip()
+      price_w_tax = table_items[3].text[2:].strip()
+      tax = table_items[4].text[2:].strip()
+      availability = only_num(table_items[5].text)
+      number_of_reviews = table_items[6].text
+      write_to_csv([title, price, stock, category, cover, upc, product_type, price_wo_tax, price_w_tax, tax, availability, number_of_reviews])
+    i += 1
 
-print(f'Se procesarion {line_count} libros.')
+  print(f'Se procesarion {line_count} libros.')
+
+if __name__ == '__main__':
+    main()
